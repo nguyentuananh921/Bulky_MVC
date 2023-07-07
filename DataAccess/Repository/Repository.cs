72972b571c85aadf33846ac99841b2fa2ec3c5ee
nguyentuananh921 <nguyentuananh921@gmail.com>
2023-07-07 14:12:15 +0700

    Section 5 - Implement Category Repository Time: 5:15:52

--- DataAccess/Repository/Repository.cs
+++ DataAccess/Repository/Repository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository
 {
-    internal class Repository<T> : IRepository<T> where T : class
+    public class Repository<T> : IRepository<T> where T : class
     {
         private ApplicationDbContext _db;
         internal DbSet<T> dbSet;
