0a53def3a25d4c03d5a403f986de67a3c820da84
nguyentuananh921 <nguyentuananh921@gmail.com>
2023-07-18 13:23:49 +0700

    Section 6 - Update Product Custom Code Time 7:16:27

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -102,13 +102,37 @@ namespace BulkyBook.Web.Areas.Admin.Controllers
                 {
                     string filename =Guid.NewGuid().ToString()+Path.GetExtension(file.FileName);
                     string productPath =Path.Combine(wwwRootPath, @"images\product");
+                    if(string.IsNullOrEmpty(productVM.Product.ImageUrl) ) 
+                    {
+                        //Delete old image
+                        if (productVM.Product.ImageUrl != null) //Make sure old ImageUrl not null 
+                        {
+                            var oldImagePath =
+                            Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                            if (System.IO.File.Exists(oldImagePath))
+                            {
+                                System.IO.File.Delete(oldImagePath);
+                            }
+                        }
+                        
+                    }
+
                     using (var fileStream = new FileStream(Path.Combine(productPath, filename), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
                     }
-                    productVM.Product.ImageUrl = @"images\product" + filename;
+                    productVM.Product.ImageUrl = @"\images\product\" + filename;
                 }
-                _unitOfWork.ProductRepository.Add(productVM.Product);
+
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.ProductRepository.Add(productVM.Product);//Add new Product
+                }
+                else
+                {
+                    _unitOfWork.ProductRepository.Update(productVM.Product);//Update product
+                }                
                 _unitOfWork.Save();  //Save to database
 
                 TempData["success"] = "Product Created successfully";//TempData with the keyname of success
